2f6fc1cef74c8612df0691529e97562713d9dddb
Kusuma123-glitch <kusumapatarapalli@gmail.com>
2025-03-31 12:46:45 +0530

    Add files via upload

---
+++ DemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
